ae607f3af7f443fbc592df1ad612b9919c496bca
Mike Martins <106335316+mikedmartins@users.noreply.github.com>
2023-04-10 11:02:01 -0300

    Update GerarSenha.py
    Este é um script Python que gera uma senha aleatória com base nas preferências do usuário. Ele solicita ao usuário que informe o comprimento desejado da senha e o tipo de caracteres a incluir (letras minúsculas, letras maiúsculas, dígitos e/ou pontuação). Em seguida, o script gera uma senha com o comprimento e o tipo de caracteres especificados, usando os módulos random e string. O código foi atualizado para incluir validação de entrada e usa elif em vez de múltiplas declarações if para melhor legibilidade.

--- GerarSenha.py
+++ GerarSenha.py
@@ -1,41 +1,28 @@
-from shutil import register_unpack_format
 import string
-
-string.ascii_lowercase # abcdefghijklmnopqrstuvwxyz
-string.ascii_uppercase # ABCDEFGHIJKLMNOPQRSTUVWXYZ
-string.ascii_letters # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-string.digits # 0123456789
-string.punctuation # <=>?@[\]^_`{|}~.
-
-from random import choice
-import string
+import random
 
 while True:
-    tamanho = int(input ('Digite a quantidade de caracteres da senha: '))
-
+    tamanho = int(input('Digite a quantidade de caracteres da senha: '))
     print('Digite 1 somente letras minusculas')
     print('Digite 2 para letras minuscolas e maiusculas')
     print('Digite 3 para letras e números')
     print('Digite 4 para letras, números e caracteres')
     tipo = int(input())
-    valores = ()
+
     if tipo == 1:
         valores = string.ascii_lowercase
         break
-    if tipo == 2:
-        valores = string.ascii_lowercase + string.ascii_uppercase
+    elif tipo == 2:
+        valores = string.ascii_letters
         break
-    if tipo == 3:
-        valores = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    elif tipo == 3:
+        valores = string.ascii_letters + string.digits
         break
-    if tipo == 4:
-        valores = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    elif tipo == 4:
+        valores = string.ascii_letters + string.digits + string.punctuation
         break
     else:
-        print('Digite alguma alternativa solicitada')  
+        print('Digite uma alternativa válida.')
 
-senha = ''
-for i in range(tamanho):
-    senha += choice(valores)
-
+senha = ''.join(random.choice(valores) for _ in range(tamanho))
 print(senha)
